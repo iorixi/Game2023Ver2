e23e59b00b971490e7831e5f2cfc1618bfb87446
MishimaIori <iori_ma3003@i.softbank.jp>
2024-02-16 06:49:36 +0900

    Fix:バグ処理

--- gameObject.h
+++ gameObject.h
@@ -14,7 +14,7 @@ protected:
 
 	DirectX::SimpleMath::Vector3	m_Position = DirectX::SimpleMath::Vector3(0.0f, 0.0f, 0.0f);
 	DirectX::SimpleMath::Vector3	m_Rotation = DirectX::SimpleMath::Vector3(0.0f, 0.0f, 0.0f);
-	DirectX::SimpleMath::Vector3	m_Scale = DirectX::SimpleMath::Vector3(3.0f, 3.0f, 3.0f);
+	DirectX::SimpleMath::Vector3	m_Scale = DirectX::SimpleMath::Vector3(1.0f, 1.0f, 1.0f);
 
 	std::list<Component*> m_Component;
 
